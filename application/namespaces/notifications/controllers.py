be508bd68bec3e445a563260d8bbc2d127a26605
Edison <edisonstuart@gmail.com>
2023-10-23 16:55:10 -0400

    Set up db and config files. Created base notification type.

--- application/namespaces/notifications/controllers.py
+++ application/namespaces/notifications/controllers.py
@@ -1,3 +1,25 @@
+import orodha_keycloak
+from application.config import obtain_config
+from application.namespaces.notifications.models import Notification
+
+APPCONFIG = obtain_config()
+
+def _create_keycloak_client() -> orodha_keycloak.OrodhaKeycloakClient:
+    """
+    Helper function which creates our keycloak client from config data
+    for use in our main controller functions.
+
+    Returns:
+        OrodhaKeycloakClient: A facade client used by Orodha in order to make interactions with
+            keycloak more uniform for the service.
+    """
+    return orodha_keycloak.OrodhaKeycloakClient(
+        server_url=APPCONFIG["keycloak_config"]["keycloak_server_url"],
+        realm_name=APPCONFIG["keycloak_config"]["keycloak_realm_name"],
+        client_id=APPCONFIG["keycloak_config"]["keycloak_client_id"],
+        client_secret_key=APPCONFIG["keycloak_config"]["keycloak_client_secret_key"],
+    )
+
 def get_notifications(token: str):
     pass
 
--- application/namespaces/notifications/models.py
+++ application/namespaces/notifications/models.py
@@ -1,4 +1,15 @@
-from mongoengine import Document, DictField
+"""Module which contains the Mongoengine document definitions."""
+from mongoengine import Document, StringField, ListField
 
-class Notification(Document):
-    data = DictField(required=True)
+class BaseNotification(Document):
+    """
+    A base document used for defining future notification types with varying behavior.
+    Contains targets which is a listField meant to contain user_id values.
+    """
+    targets = ListField(required=True)
+
+class ListInviteNotification(BaseNotification):
+    """
+    A list invite notification. Contains the list_id of the target list
+    """
+    list_id = StringField(required=True)
